e3c43145ceec97511a26fffe5367c86a4f4b6649
Himalaya Pal <himalayapal123@gmail.com>
2022-05-15 20:20:49 +0545

    upload model

---
+++ models.py/caulipy.py
@@ -0,0 +1,257 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error,r2_score,mean_absolute_error
+import pickle
+
+
+# ## Get the data
+
+# In[20]:
+
+
+data = pd.read_csv(r"C:\Users\himal\OneDrive\Desktop\Django\crashcourse\crm1\accounts\csv\Cauli_cleaned_data.csv")
+data.head(5)
+
+
+# ## Node class
+
+# In[21]:
+
+
+class Node():
+    def __init__(self, feature_index=None, threshold=None, left=None, right=None, var_red=None, value=None):
+        ''' constructor ''' 
+        
+        # for decision node
+        self.feature_index = feature_index
+        self.threshold = threshold
+        self.left = left
+        self.right = right
+        self.var_red = var_red
+        
+        # for leaf node
+        self.value = value
+
+
+# ## Tree class
+
+# In[22]:
+
+
+class DecisionTreeRegressor():
+    def __init__(self, min_samples_split=2, max_depth=2):
+        ''' constructor '''
+        
+        # initialize the root of the tree 
+        self.root = None
+        
+        # stopping conditions
+        self.min_samples_split = min_samples_split
+        self.max_depth = max_depth
+        
+    def build_tree(self, dataset, curr_depth=0):
+        ''' recursive function to build the tree '''
+        
+        X, Y = dataset[:,:-1], dataset[:,-1]
+        num_samples, num_features = np.shape(X)
+        best_split = {}
+        # split until stopping conditions are met
+        if num_samples>=self.min_samples_split and curr_depth<=self.max_depth:
+            # find the best split
+            best_split = self.get_best_split(dataset, num_samples, num_features)
+            # check if information gain is positive
+            if best_split["var_red"]>0:
+                # recur left
+                left_subtree = self.build_tree(best_split["dataset_left"], curr_depth+1)
+                # recur right
+                right_subtree = self.build_tree(best_split["dataset_right"], curr_depth+1)
+                # return decision node
+                return Node(best_split["feature_index"], best_split["threshold"], 
+                            left_subtree, right_subtree, best_split["var_red"])
+        
+        # compute leaf node
+        leaf_value = self.calculate_leaf_value(Y)
+        # return leaf node
+        return Node(value=leaf_value)
+    
+    def get_best_split(self, dataset, num_samples, num_features):
+        ''' function to find the best split '''
+        
+        # dictionary to store the best split
+        best_split = {}
+        max_var_red = -float("inf")
+        # loop over all the features
+        for feature_index in range(num_features):
+            feature_values = dataset[:, feature_index]
+            possible_thresholds = np.unique(feature_values)
+            # loop over all the feature values present in the data
+            for threshold in possible_thresholds:
+                # get current split
+                dataset_left, dataset_right = self.split(dataset, feature_index, threshold)
+                # check if childs are not null
+                if len(dataset_left)>0 and len(dataset_right)>0:
+                    y, left_y, right_y = dataset[:, -1], dataset_left[:, -1], dataset_right[:, -1]
+                    # compute information gain
+                    curr_var_red = self.variance_reduction(y, left_y, right_y)
+                    # update the best split if needed
+                    if curr_var_red>max_var_red:
+                        best_split["feature_index"] = feature_index
+                        best_split["threshold"] = threshold
+                        best_split["dataset_left"] = dataset_left
+                        best_split["dataset_right"] = dataset_right
+                        best_split["var_red"] = curr_var_red
+                        max_var_red = curr_var_red
+                        
+        # return best split
+        return best_split
+    
+    def split(self, dataset, feature_index, threshold):
+        ''' function to split the data '''
+        
+        dataset_left = np.array([row for row in dataset if row[feature_index]<=threshold])
+        dataset_right = np.array([row for row in dataset if row[feature_index]>threshold])
+        return dataset_left, dataset_right
+    
+    def variance_reduction(self, parent, l_child, r_child):
+        ''' function to compute variance reduction '''
+        
+        weight_l = len(l_child) / len(parent)
+        weight_r = len(r_child) / len(parent)
+        reduction = np.var(parent) - (weight_l * np.var(l_child) + weight_r * np.var(r_child))
+        return reduction
+    
+    def calculate_leaf_value(self, Y):
+        ''' function to compute leaf node '''
+        
+        val = np.mean(Y)
+        return val
+                
+    def print_tree(self, tree=None, indent=" "):
+        ''' function to print the tree '''
+        
+        if not tree:
+            tree = self.root
+
+        if tree.value is not None:
+            print(tree.value)
+
+        else:
+            print("X_"+str(tree.feature_index), "<=", tree.threshold, "?", tree.var_red)
+            print("%sleft:" % (indent), end="")
+            self.print_tree(tree.left, indent + indent)
+            print("%sright:" % (indent), end="")
+            self.print_tree(tree.right, indent + indent)
+    
+    def fit(self, X, Y):
+        ''' function to train the tree '''
+        
+        dataset = np.concatenate((X, Y), axis=1)
+        self.root = self.build_tree(dataset)
+        
+    def make_prediction(self, x, tree):
+        ''' function to predict new dataset '''
+        
+        if tree.value!=None: return tree.value
+        feature_val = x[tree.feature_index]
+        if feature_val<=tree.threshold:
+            return self.make_prediction(x, tree.left)
+        else:
+            return self.make_prediction(x, tree.right)
+    
+    def predict(self, X):
+        ''' function to predict a single data point '''
+        
+        preditions = [self.make_prediction(x, self.root) for x in X]
+        return preditions
+
+
+# ## Train-Test split
+
+# In[23]:
+
+
+X = data[["Day","Month","Year"]].values
+Y = data["Price"].values.reshape(-1,1)
+
+x_train, x_test, y_train, y_test = train_test_split(X,Y,train_size=0.8,shuffle=True, random_state=1)
+
+x_train.shape
+x_test.shape
+
+
+# ## Fit the model
+
+# In[24]:
+
+
+regressor = DecisionTreeRegressor(min_samples_split=3, max_depth=3)
+regressor.fit(x_train,y_train)
+
+
+# ## Test the model
+
+# In[25]:
+
+
+y_predict = regressor.predict(x_test) 
+
+from sklearn.preprocessing import StandardScaler
+np.sqrt(mean_squared_error(y_test, y_predict))
+
+
+# In[26]:
+
+
+dframe = pd.DataFrame(y_test,y_predict).reset_index()
+
+
+# In[27]:
+
+
+scaler = StandardScaler()
+
+
+# In[28]:
+
+
+scaler.fit(x_train)
+
+
+# In[29]:
+
+
+dframe.columns = ['Actual Price', 'Predicted Price']
+dfr=dframe
+dfr
+
+
+# In[30]:
+
+
+
+print("Mean squared error is:", mean_squared_error(y_test,y_predict))
+print("Absolute Error is:", mean_absolute_error(y_test,y_predict))
+
+
+# In[31]:
+
+
+import matplotlib.pyplot as plt
+graph = dfr.head(20)
+graph.plot(kind='bar')
+
+
+# In[33]:
+
+
+pickle.dump(regressor, open('model.pkl', 'wb'))
+
+
+# In[34]:
+
+
+for data in regressor.predict([[2,3,2022]]):
+    price = data
+print("Predicted price of cauli  is Rs ",data)
+
